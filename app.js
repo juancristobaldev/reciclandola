6efde8a07ad77fc03236401c84cfd995a55d14b7
juancri <juancristeam@gmail.com>
2021-04-07 23:14:26 -0400

    fix

--- app.js
+++ app.js
@@ -7,11 +7,13 @@
 // server.listen(puerto, () => {
 //     console.log('Recibiendo solicitudes.')
 // })
- 
+
 const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"))
 
 app.use((req, res, next) =>{
     res.status(404).sendFile(__dirname + "/public/404.html")
