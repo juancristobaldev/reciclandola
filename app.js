a575bf172d9e7d668f2b1f1cb45c719bc9bf4876
juancri <juancristeam@gmail.com>
2021-04-07 21:40:28 -0400

    fix

---
+++ app.js
@@ -0,0 +1,25 @@
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//     res.end('Estoy respondiendo tus solicitudes v3')
+// })
+
+// const puerto = 3000;
+// server.listen(puerto, () => {
+//     console.log('Recibiendo solicitudes.')
+// })
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"))
+
+app.use((req, res, next) =>{
+    res.status(404).sendFile(__dirname + "/public/404.html")
+})
+
+
+app.listen(port, () =>{
+    console.log('Servidor a tu servicio en puerto ', port)
+})
---
+++ public/js/js.js
@@ -0,0 +1,75 @@
+
+document.addEventListener('DOMContentLoaded',() => {
+    let linkmenu = document.querySelectorAll("#linkmenu");
+    let headerbackground = document.getElementById("nav");
+    let tiendalink = document.getElementById("tiendalink");
+    let anim = document.querySelectorAll("#anim");
+    let galeria = document.getElementById("galeria");
+
+    function Animaciones(){
+        var i;
+        let PixelesRecorridos = document.documentElement.scrollTop;
+        for (i=0; i < linkmenu.length; i++){
+            if (300 < PixelesRecorridos){
+                headerbackground.style.backgroundColor = "white";
+                linkmenu[i].style.color = "green";
+                tiendalink.style.border = "solid 1px green";
+                tiendalink.style.color = "green";
+                
+            }
+            else if (300 > PixelesRecorridos){
+                headerbackground.style.backgroundColor = "transparent";
+                linkmenu[i].style.color = "white";
+                tiendalink.style.border = "solid 1px white";
+                tiendalink.style.color = "white";
+            }
+        }
+        for (i=0; i < anim.length; i++){
+            let altura = anim[i].offsetTop;
+            let maxwidth = galeria.offsetWidth;
+            if (altura - 900 < PixelesRecorridos){
+                anim[i].style.transform = "translateX(0px)";
+            }
+            else if ( altura + 100 > PixelesRecorridos && maxwidth < 800 ){
+                anim[0].style.transform = "translateX(-50vw)";
+                anim[1].style.transform = "translateX(50vw)";
+                anim[2].style.transform = "translateX(50vw)";
+            }
+            else if (altura < PixelesRecorridos && maxwidth > 800){
+                anim[i].style.transform = "translatey(50vw)";
+            }
+            else if (altura + 500 > PixelesRecorridos && maxwidth > 800){
+                anim[i].style.transform = "translatey(50vh)";
+            }
+
+        }
+    }
+
+    window.addEventListener("scroll", Animaciones)
+    
+    let article = document.getElementsByClassName("article-vasos");
+    let articleright = document.getElementsByClassName("article-bong")
+    let btnleft = document.getElementsByClassName("buton-left")
+    let btnright = document.getElementsByClassName("buton-right")
+    document.querySelector('#btnleft').addEventListener('click', function(){
+        
+            for(i = 0; i < article.length; i++){
+                article[i].style.transform = 'translatex(0px)';
+                articleright[i].style.transform = "translatex(0px)";
+                btnleft[i].style.width = "20px";
+                btnright[i].style.width = "8px";
+            }
+    });
+    document.querySelector('#btnright').addEventListener('click', function(){
+        for(i = 0; i < article.length; i++){
+            article[i].style.transform = 'translatex(-67.5vw)';
+            articleright[i].style.transform = "translatex(-70vw)";
+            btnright[i].style.width = "20px";
+            btnleft[i].style.width = "8px";
+            
+        }
+    });
+});
+
+
+
