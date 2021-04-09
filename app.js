192fa6aaa88526abc3d12f468ad6becb27d28975
juancri <juancristeam@gmail.com>
2021-04-09 01:23:44 -0400

    boton

--- app.js
+++ app.js
@@ -25,25 +25,7 @@ app.get('/login' , (req,res) => {
     })
     
 })
-FB.getLoginStatus(function(response) {
-    if (response.status === 'connected') {
-      // The user is logged in and has authenticated your
-      // app, and response.authResponse supplies
-      // the user's ID, a valid access token, a signed
-      // request, and the time the access token 
-      // and signed request each expire.
-      var uid = response.authResponse.userID;
-      var accessToken = response.authResponse.accessToken;
-    } else if (response.status === 'not_authorized') {
-      // The user hasn't authorized your application.  They
-      // must click the Login button, or you must call FB.login
-      // in response to a user gesture, to launch a login dialog.
-    } else {
-      // The user isn't logged in to Facebook. You can launch a
-      // login dialog with a user gesture, but the user may have
-      // to log in to Facebook before authorizing your application.
-    }
-   });
+
 
 app.use((req, res, next) =>{
     res.status(404).sendFile(__dirname + "/public/404.html")
