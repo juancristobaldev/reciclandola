67842202041b78cf58d4a0364cfb5ce1ca9f76bd
juancri <juancristeam@gmail.com>
2021-04-15 21:59:38 -0400

    fix

--- public/js/js.js
+++ public/js/js.js
@@ -4,35 +4,36 @@
 
 
 document.addEventListener('DOMContentLoaded',() => {
+    window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '{your-app-id}',
+          cookie     : true,
+          xfbml      : true,
+          version    : '{api-version}'
+        });
+          
+        FB.AppEvents.logPageView();  
+        FB.getLoginStatus(function(response) {
+      statusChangeCallback({ 
+          status: 'connected',
+          authResponse: {
+          accessToken: '...',
+          expiresIn:'...',
+          signedRequest:'...',
+          userID:'...'
+      }});
+      }); 
+          
+      };
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "https://connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
     document.querySelector("#loginbutton").addEventListener('click',function(){
-        window.fbAsyncInit = function() {
-            FB.init({
-              appId      : '{your-app-id}',
-              cookie     : true,
-              xfbml      : true,
-              version    : '{api-version}'
-            });
-              
-            FB.AppEvents.logPageView();  
-            FB.getLoginStatus(function(response) {
-          statusChangeCallback({ 
-              status: 'connected',
-              authResponse: {
-              accessToken: '...',
-              expiresIn:'...',
-              signedRequest:'...',
-              userID:'...'
-          }});
-          }); 
-              
-          };
-          (function(d, s, id){
-             var js, fjs = d.getElementsByTagName(s)[0];
-             if (d.getElementById(id)) {return;}
-             js = d.createElement(s); js.id = id;
-             js.src = "https://connect.facebook.net/en_US/sdk.js";
-             fjs.parentNode.insertBefore(js, fjs);
-           }(document, 'script', 'facebook-jssdk'));
+        FB.login()
     })
     
 
