10e3eeb742d49c34c163e689b78c7e545e33ce48
juancri <juancristeam@gmail.com>
2021-04-15 21:58:09 -0400

    fix

--- public/js/js.js
+++ public/js/js.js
@@ -1,5 +1,49 @@
 
+
+
+
+
 document.addEventListener('DOMContentLoaded',() => {
+    document.querySelector("#loginbutton").addEventListener('click',function(){
+        window.fbAsyncInit = function() {
+            FB.init({
+              appId      : '{your-app-id}',
+              cookie     : true,
+              xfbml      : true,
+              version    : '{api-version}'
+            });
+              
+            FB.AppEvents.logPageView();  
+            FB.getLoginStatus(function(response) {
+          statusChangeCallback({ 
+              status: 'connected',
+              authResponse: {
+              accessToken: '...',
+              expiresIn:'...',
+              signedRequest:'...',
+              userID:'...'
+          }});
+          }); 
+              
+          };
+          (function(d, s, id){
+             var js, fjs = d.getElementsByTagName(s)[0];
+             if (d.getElementById(id)) {return;}
+             js = d.createElement(s); js.id = id;
+             js.src = "https://connect.facebook.net/en_US/sdk.js";
+             fjs.parentNode.insertBefore(js, fjs);
+           }(document, 'script', 'facebook-jssdk'));
+    })
+    
+
+
+
+
+
+
+
+
+
     let article = document.getElementsByClassName("article-vasos");
     let articleleft = document.getElementById("article-vasos")
     let articleright = document.getElementById("article-bong")
